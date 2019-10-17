c1011a1409598ab2ab1a0a9c1fbbf4c0429c1466
Ricardo Kagawa <ricardo.kagawa@gmail.com>
2019-10-17 16:42:08 -0300

    Fixed database URL

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var request = require("request");
 app.get('/', function (req, res) {
     var options = {
         method: 'GET',
-      url: `https://r4ypabckgnf26cc-workshopatp.adb.us-ashburn-1.oraclecloudapps.com/ords/admin/soda/latest/ricardo/?id=${Math.floor(Math.random()*1000)}`,
+      url: `https://r4ypabckgnf26cc-workshopatp.adb.us-ashburn-1.oraclecloudapps.com/ords/ords/api/users/?id=${Math.floor(Math.random()*1000)}`,
     };
     request(options, function (error, response, body) {
         if (error) throw new Error(error);
